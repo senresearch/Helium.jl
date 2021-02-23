303d42d7410f0ce882da9a02ab8689bee493a92d
Gregory Farage <gfarage@uthsc.edu>
2021-02-23 00:34:28 -0600

    update Readme and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,19 @@
 push!(LOAD_PATH,"../src/")
 using Helium
 using Documenter
+
+const src = "https://github.com/senresearch/Helium.jl"
+const dst = "https://senresearch.github.io/Helium.jl/stable"
+
 makedocs(
          sitename = "Helium.jl",
+         authors = "Gregory Farage",
          modules  = [Helium],
          pages=[
-                "Home" => "index.md"
+                "Home" => "index.md",
+                "Function Documentation" => "functionDoc.md"
                ])
 deploydocs(;
     repo="github.com/senresearch/Helium.jl",
+    devurl="stable",
 )
